ff48e149aeeb5c28c798062fc06a989f4c5cfacd
prince-conte <prince-conte@outlook.com>
2017-01-15 16:53:39 +0300

    init + yap (1.1.1)

--- tars-master/markup/static/js/separate-js/map.js
+++ tars-master/markup/static/js/separate-js/map.js
@@ -11,12 +11,7 @@ map.addControl(new YMaps.TypeControl());
 map.addControl(new YMaps.ToolBar());
 map.addControl(new YMaps.Zoom());
 map.addControl(new YMaps.ScaleLine());        
-    
-// Создает метку в центре Москвы
-var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.595117, 55.653279));
-
-
-        
+   
         
         
 // Создает стиль
@@ -28,13 +23,17 @@ s.iconStyle = new YMaps.IconStyle();
 
 s.iconStyle.href = "static/img/general/metka.png";
 s.iconStyle.size = new YMaps.Point(54, 64);
-s.iconStyle.offset = new YMaps.Point(-109, -95);
-          
+s.iconStyle.offset = new YMaps.Point(-109, -95);     
         
         
-       // Создает метку в центре Москвы
-var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.595117, 55.653279) , {style: s});
+// Создает метку в центре Москвы
+var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.595117, 55.653279) , {
+    style: s});
+        
+placemark.setBalloonContent("<b>Школа татуажа Елены Нечаевой</b>");
     
+        
+  
         
 // Добавляет метку на карту
 map.addOverlay(placemark);  
